1bd5d38e4c2eab29dd1631a692ddbcf4b36632a7
mijelpalcabello <miguel.castillo861999@gmail.com>
2022-07-22 21:09:51 -0500

    Agregando acrylic

--- index.js
+++ index.js
@@ -1,30 +1,61 @@
-const { BrowserWindow, app, ipcMain, dialog } = require('electron')
+const { app, ipcMain, dialog, Notification, Menu } = require('electron')
+const { BrowserWindow } = require('electron-acrylic-window')
 const path = require('path')
 const fs = require('fs')
 
-require('electron-reloader')(module)
+const isDevEnv = process.env.NODE_ENV === 'development'
+
+if (isDevEnv) {
+	try {
+		require('electron-reloader')(module)
+	} catch (e){
+		console.log(e)
+	}
+}
 
 let mainWindow
+let openedFilePath
 
 const createWindow = () => {
 	mainWindow = new BrowserWindow({
-		transparency: true,
-		backgroundColor: '#000000',
-		vibrancy: 'under-window',
-		visualEffectState: 'active',
 		width: 900,
 		height: 700,
+		frame: false,
+		vibrancy: 'dark',
 		titleBarStyle: 'hiddenInset',
 		webPreferences: {
 			preload: path.join(app.getAppPath(), 'renderer.js')
 		}
 	})
 
-	mainWindow.webContents.openDevTools()
+	if(isDevEnv) {
+		mainWindow.webContents.openDevTools()
+	}
+
 	mainWindow.loadFile('index.html')
+
+	const menuTemplate = [{
+		label: 'File',
+		submenu: [
+			{
+				label: 'Add new file',
+				click: () => ipcMain.emit('open-document-triggered')
+			}
+		]
+	}]
+
+	const menu = Menu.buildFromTemplate(menuTemplate)
+	Menu.setApplicationMenu(menu)
 }
 
 app.whenReady().then(createWindow)
+
+const handleError = (error) => {
+	new Notification({
+		title: 'Error',
+		body: error
+	}).show()
+}
 
 ipcMain.on('create-document-triggered', () => {
 	dialog
@@ -33,14 +64,41 @@ ipcMain.on('create-document-triggered', () => {
 		})
 		.then(({ filePath }) => {
 			console.log('file path', filePath)
-			fs.writeFile(filePath, "", error => {
+			fs.writeFile(filePath, "Primer texto", error => {
 				if(error) {
-					console.log(error)
+					handleError(error)
 				} else {
+					openedFilePath = filePath
 					mainWindow.webContents.send('document-created', filePath)
 				}
 			})
 		})
 })
 
+ipcMain.on('open-document-triggered', () => {
+	dialog
+		.showOpenDialog(mainWindow, {
+			filters: [{ name: '', extensions: ['txt', 'js'] }]
+		})
+		.then(({filePaths}) => {
+			const filePath = filePaths[0]
+
+			fs.readFile(filePath, "utf8", (error, content) => {
+				if(error) {
+					handleError(error)
+				} else {
+					openedFilePath = filePath
+					mainWindow.webContents.send('document-opened', { filePath, content})
+				}
+			})
+		})
+})
+
+ipcMain.on('file-content-updated', (_, textareaContent) => {
+	fs.writeFile(openedFilePath, textareaContent, error => {
+		if (error) {
+			handleError(error)
+		}
+	})
+})
 
--- renderer.js
+++ renderer.js
@@ -1,9 +1,19 @@
 const { ipcRenderer } = require('electron')
+const path = require('path')
 
 window.addEventListener('DOMContentLoaded', () => {
 	const el = {
 		documentName: document.getElementById('documentName')	,
-		createDocumentButton: document.getElementById('createDocumentButton')
+		createDocumentButton: document.getElementById('createDocumentButton'),
+		openDocumentButton: document.getElementById('openDocumentButton'),
+		fileTextArea: document.getElementById('fileTextArea'),
+	}
+
+	const handleDocumentChange = (filePath, content='') => {
+		el.documentName.innerHTML = path.parse(filePath).base
+		el.fileTextArea.removeAttribute('disabled')
+		el.fileTextArea.value = content
+		el.fileTextArea.focus()
 	}
 
 	el.createDocumentButton.addEventListener('click', () => {
@@ -11,7 +21,20 @@ window.addEventListener('DOMContentLoaded', () => {
 		ipcRenderer.send('create-document-triggered')
 	})
 
+	el.openDocumentButton.addEventListener('click', () => {
+		console.log('Document opened')
+		ipcRenderer.send('open-document-triggered')
+	})
+
+	el.fileTextArea.addEventListener('input', (e) => {
+		ipcRenderer.send('file-content-updated', e.target.value)
+	})
+
 	ipcRenderer.on('document-created', (_, filePath) => {
-		el.documentName.innerHTML = filePath;	
+		handleDocumentChange(filePath)
+	})
+
+	ipcRenderer.on('document-opened', (_, {filePath, content}) => {
+		handleDocumentChange(filePath, content)
 	})
 })
